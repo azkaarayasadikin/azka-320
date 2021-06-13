8077a896e2feaf7f0488a14bf657a6f5f586b429
azkaarayasadikin <azkaarayasadikin@webmail.umm.ac.id>
2021-06-13 19:19:20 +0700

    project web

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Home</title>
+</head>
+<body>
+<div>
+<nav class="awal">
+<h1>Selamat Datang Di Keluarga <?php echo "Cemara"?></h1>
+<h1>Assalamualaikum <?php echo "Guys"?></h1>
+</div>
+
+<div>
+<ul>
+<li><a href="https://azka-320.com">azka-320</a></li>
+<li><a href="https://azkaaraya.azka-320.com">azkaaraya</a></li>
+<li><a href="https://mamangkey.azka-320.com">mamangkey</a></li>
+<li><a href="https://sadikin.azka-320.com">sadikin</a></li>
+</ul>
+</div>
+</nav>
+    
+</body>
+</html>
